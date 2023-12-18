c1649e7456240e87a2721a627bda54b43b31d17b
saularana <saularana201831189@cunoc.edu.gt>
2023-12-18 01:48:37 -0600

    en el main se creo unos botones de prueba todo ello para ver que funcione correctamente

--- main.py
+++ main.py
@@ -2,27 +2,41 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import os
+from tkinter import ttk
+from importacion.Listas import ListaDobleEnlazada
+
+lista_de_listas = []
+tu_lista_de_canciones = ListaDobleEnlazada()
+lista_de_listas.append(tu_lista_de_canciones)
 
 def expandir_ultima_columna(event):
     ancho_ventana = event.width
     frame_inferior.grid_columnconfigure(num_columnas_frame_inferior - 1, weight=1)
-    
-def agregar_items_listbox(lista_items):
+
+def agregar_items_treeview(lista_items):
     actual = lista_items.inicio
+    index = 1
     while actual:
-        listbox.insert(tk.END, actual.cancion.getnombre())
+        tree.insert("", "end", text=str(index), values=(actual.cancion.getnombre(), actual.cancion.getartista(), actual.cancion.getalbum()))
         actual = actual.siguiente
-    
-    
+        index += 1
+
+def importar_xml():
+    archivo_seleccionado = filedialog.askopenfilename(
+        title="Seleccionar Archivo",
+        filetypes=[("Archivos XML", "*.xml"), ("Todos los archivos", "*.*")]
+    )
+    if archivo_seleccionado:
+        tu_lista_de_canciones.importar_desde_xml(archivo_seleccionado)
+        # Limpia el Treeview antes de agregar nuevos elementos
+        for item in tree.get_children():
+            tree.delete(item)
+        # Agrega los elementos actualizados al Treeview
+        agregar_items_treeview(tu_lista_de_canciones)
+
 # Crear la ventana
 ventana = tk.Tk()
 ventana.bind('<Configure>', expandir_ultima_columna)
-# Establecer el tamaño de la ventana
-ventana.geometry("800x500")  # Ancho x Alto
-
-# Evitar que la ventana sea redimensionable
-ventana.resizable(width=False, height=False)
-
 ventana.title("IPC2Music")
 
 # Frame superior para la imagen y los labels
@@ -47,6 +61,7 @@ label_imagen.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
 
 frame_superior_derecha = tk.Frame(ventana)
 frame_superior_derecha.grid(row=0, column=1, padx=5, pady=5, sticky=tk.E)
+frame_superior_derecha.grid_columnconfigure(0, weight=1)
 
 # Labels
 label_artista = tk.Label(frame_superior_derecha, text="Artista:")
@@ -61,29 +76,33 @@ label_album.grid(row=2, column=1, rowspan=3, padx=5, pady=5, sticky=tk.E)
 # Frame inferior para botones y Listbox
 frame_inferior = tk.Frame(ventana)
 frame_inferior.grid(row=1, column=0, padx=5, pady=5, sticky=tk.W + tk.E + tk.N + tk.S)
+frame_inferior.grid_rowconfigure(1, weight=1)
 
 # Botones
 btn_iniciar_pausar_cancion = tk.Button(frame_inferior, text="Play")
 btn_anterior_cancion = tk.Button(frame_inferior, text="Prev")
 btn_siguiente_cancion = tk.Button(frame_inferior, text="Next")
+btn_importar_xml = tk.Button(frame_inferior, text="Importar Xml", command=importar_xml)
+btn_exportar_xml = tk.Button(frame_inferior, text="Exportar Xml")
 
 # Posicionamiento de los botones usando grid
 btn_iniciar_pausar_cancion.grid(row=0, column=0, padx=5, pady=2)
 btn_anterior_cancion.grid(row=0, column=1, padx=5, pady=2)
 btn_siguiente_cancion.grid(row=0, column=2, padx=5, pady=2)
+btn_importar_xml.grid(row=0, column=3, padx=5, pady=2)
+btn_exportar_xml.grid(row=0, column=4, padx=5, pady=2)
 
-# Listbox
-listbox = tk.Listbox(frame_inferior)
-listbox.grid(row=1, column=0, columnspan=3, padx=5, pady=5, sticky=tk.W + tk.E)
+# Treeview
+tree = ttk.Treeview(frame_inferior, columns=("Nombre", "Artista", "Álbum"))
+tree.grid(row=1, column=0, columnspan=3, padx=5, pady=5, sticky=tk.W + tk.E)
 
-# Agregar elementos al Listbox (puedes personalizar esto)
-listbox.insert(tk.END, "Canción 1")
-listbox.insert(tk.END, "Canción 2")
-listbox.insert(tk.END, "Canción 3")
+# Configurar encabezados de columnas
+tree.heading("#0", text="Índice")
+tree.heading("Nombre", text="Nombre")
+tree.heading("Artista", text="Artista")
+tree.heading("Álbum", text="Álbum")
 
-# Iniciar el bucle principal de Tkinter
-num_columnas_frame_inferior = frame_inferior.grid_size()[0]
-print("Número de columnas en el frame_inferior:", num_columnas_frame_inferior)
-
+# Agregar elementos al Treeview (puedes personalizar esto)
+agregar_items_treeview(tu_lista_de_canciones)
 
 ventana.mainloop()
